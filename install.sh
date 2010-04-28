b25b56e1a058b0683365a668824c00560568f470
Jens Låås <jens@laas.mine.nu>
2010-04-28 16:31:11 +0200

    install script for installation of the framework

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script for installing the framework to a destination directory.
+# This includes: copying files, creating links, creating directory structure.
+# Everything should be put inplace so that the distribution binaries may be
+# unpacked on-top of the structure to create a complete system.
